d171c7228209aa9abeacd72a8dd3fc86d71a6fae
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-02-24 18:36:47 +0000

    Add file

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * times_table - prints the 9 times table
+ *
+ * Return: void
+ */
+void times_table(void)
+{
+	int i;
+	int j;
+	int result;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			result = i * j;
+			if (j != 0)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+			if (result >= 10)
+				_putchar('0' + result / 10);
+			else if (j != 0)
+				_putchar(' ');
+			_putchar('0' + result % 10);
+		}
+		_putchar('\n');
+	}
+}
