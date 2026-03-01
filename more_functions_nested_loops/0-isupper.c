09db95af2294a9efac2378eacc3bba3f8944cd42
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-03-01 18:37:07 +0000

    fix

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -6,10 +6,9 @@
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-	if (c>='A' && c <= 'Z')
-		return(1);
-	return(0);
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
 }
