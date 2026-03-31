82aa5a982b577082e33bb1b81ed6a9b1a24ce23c
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-03-31 13:17:50 +0000

    Add file

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -7,12 +7,14 @@
  * @age: the age of the dog
  * @owner: the owner of the dog
  */
-typedef struct dog
+struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
