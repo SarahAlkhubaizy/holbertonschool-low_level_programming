36efff3b19524ecfda7e4cee825665bc51a68e58
SarahAlkhubaizy <sarahalkhubaizy@gmail.com>
2026-04-05 13:10:11 +0000

    Add file

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -3,9 +3,48 @@
 #include <stdio.h>
 
 /**
+ * print_char - prints a char
+ * @args: va_list
+ */
+static void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an int
+ * @args: va_list
+ */
+static void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float
+ * @args: va_list
+ */
+static void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_str - prints a string
+ * @args: va_list
+ */
+static void print_str(va_list args)
+{
+	char *str;
+
+	str = va_arg(args, char *);
+	printf("%s", str ? str : "(nil)");
+}
+
+/**
  * print_all - prints anything
  * @format: list of types of arguments
- * @...: the arguments
+ * @...: the arguments to print
  *
  * Return: void
  */
@@ -13,49 +52,39 @@ void print_all(const char * const format, ...)
 {
 	va_list args;
 	unsigned int i;
-	int sep;
-	char *str;
+	unsigned int printed;
+	unsigned int j;
+	void (*funcs[4])(va_list);
+	char keys[4];
 
-	va_start(args, format);
+	keys[0] = 'c';
+	keys[1] = 'i';
+	keys[2] = 'f';
+	keys[3] = 's';
+	funcs[0] = print_char;
+	funcs[1] = print_int;
+	funcs[2] = print_float;
+	funcs[3] = print_str;
 	i = 0;
-	sep = 0;
+	printed = 0;
+	va_start(args, format);
 	while (format && format[i])
 	{
-		str = NULL;
-		if (format[i] == 'c')
+		j = 0;
+		while (j < 4)
 		{
-			if (sep)
-				printf(", ");
-			printf("%c", va_arg(args, int));
-			sep = 1;
-		}
-		if (format[i] == 'i')
-		{
-			if (sep)
-				printf(", ");
-			printf("%d", va_arg(args, int));
-			sep = 1;
-		}
-		if (format[i] == 'f')
-		{
-			if (sep)
-				printf(", ");
-			printf("%f", va_arg(args, double));
-			sep = 1;
-		}
-		if (format[i] == 's')
-		{
-			if (sep)
-				printf(", ");
-			str = va_arg(args, char *);
-			if (str == NULL)
-				printf("(nil)");
-			else
-				printf("%s", str);
-			sep = 1;
+			if (format[i] == keys[j])
+			{
+				if (printed)
+					printf(", ");
+				funcs[j](args);
+				printed++;
+				break;
+			}
+			j++;
 		}
 		i++;
 	}
-	printf("\n");
 	va_end(args);
+	printf("\n");
 }
